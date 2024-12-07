0df294ea96aad71ccf3becd1df1223ed38d5c988
Charlie Hagedorn <charlie@hagedorn.xyz>
2024-12-06 21:06:12 -0800

    Adds a forecast script

---
+++ forecast.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+import urllib3
+
+
+forecast_site = "https://nwac.us/mountain-weather-forecast/"
+
+http = urllib3.PoolManager()
+
+page = http.request('GET',forecast_site)
+
+soup = BeautifulSoup(page.data)
+
+preamble = "<html>"
+footer = "</html>"
+forecast_date = soup.find_all("div", {"class" : "forecast-info"})
+synopsis = soup.find_all("div", {"class" : "synopsis"})
+print(preamble +str(forecast_date[0])+str(synopsis[0])+ footer)
+
